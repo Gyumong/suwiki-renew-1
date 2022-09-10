67b397c3540195fc5586991af315bdd8a6a7a45a
truewayy <dldudwls888@gmail.com>
2022-09-10 19:01:47 +0900

    feat: 메인, 로그인 페이지 url 연동

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,11 +1,21 @@
+import Link from "next/link";
+
 const NavigationBar = () => {
   return (
     <>
       <nav className="container">
-        <img src="/assets/img/LOGO.png" />
+        <Link href="/">
+          <a>
+            <img src="/assets/img/LOGO.png" />
+          </a>
+        </Link>
         <ul>
           <li className="notice">공지사항</li>
-          <li className="login">로그인</li>
+          <Link href="/login">
+            <a>
+              <li className="login">로그인</li>
+            </a>
+          </Link>
           <li className="signup">회원가입</li>
         </ul>
       </nav>
@@ -29,6 +39,11 @@ const NavigationBar = () => {
         }
         .signup {
           color: #346cfd;
+        }
+        a,
+        a:visited {
+          text-decoration: none;
+          color: black;
         }
       `}</style>
     </>
